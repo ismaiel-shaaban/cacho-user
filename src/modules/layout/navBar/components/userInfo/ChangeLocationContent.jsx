f7e3cdec30b869b6365351b638da1c27c882a057
AnasHagras <anas.hagras1999@gmail.com>
2024-06-04 04:47:00 +0300

    edit map

--- src/modules/layout/navBar/components/userInfo/ChangeLocationContent.jsx
+++ src/modules/layout/navBar/components/userInfo/ChangeLocationContent.jsx
@@ -1,15 +1,16 @@
-import {GoogleMap, Marker, useJsApiLoader} from "@react-google-maps/api";
-import GooglePlacesAutocomplete, {geocodeByAddress, getLatLng} from 'react-google-places-autocomplete';
-import {memo, useCallback, useEffect, useState} from "react";
-import {fetchLocation} from "@/utilis/getUserLocation";
-import {strings} from "@/utilis/Localization";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import GooglePlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-google-places-autocomplete';
+import { memo, useCallback, useEffect, useState } from "react";
+import { fetchLocation } from "@/utilis/getUserLocation";
+import { strings } from "@/utilis/Localization";
 
-const ChangeLocationContent = ({onLocationChange}) => {
+const ChangeLocationContent = ({ onLocationChange }) => {
     const [userLocation, setUserLocation] = useState("");
     const [value, setValue] = useState(null);
-    const {isLoaded} = useJsApiLoader({
+    const { isLoaded } = useJsApiLoader({
         id: 'google-map-script', googleMapsApiKey: "AIzaSyBhs9awrQC82lygPiy4Cq91xyX9s3WUjUI", libraries: ["places"]
     })
+
     const [center, setCenter] = useState({
         lat: parseFloat(localStorage.getItem("latitude")), lng: parseFloat(localStorage.getItem("longitude"))
     })
@@ -17,8 +18,8 @@ const ChangeLocationContent = ({onLocationChange}) => {
 
     const onLoad = useCallback(function callback(map) {
         // This is just an example of getting and using the map instance!!! don't just blindly copy!
-        const bounds = new window.google.maps.LatLngBounds(center);
-        map.fitBounds(bounds);
+        // const bounds = new window.google.maps.LatLngBounds(center);
+        // map.fitBounds(bounds);
 
         setMap(map)
         // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -41,38 +42,55 @@ const ChangeLocationContent = ({onLocationChange}) => {
         });
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [center]);
-    console.log("Value" , value)
+    console.log("Value", value)
     useEffect(() => {
-        if(value){
+        if (value) {
             geocodeByAddress(value.value.description)
                 .then(results => getLatLng(results[0]))
                 .then(({ lat, lng }) =>
-                    setCenter({lat , lng})
+                    setCenter({ lat, lng })
                 );
         }
     }, [value]);
+    function placeMarker(loc) {
 
-    return (<div>
-            {isLoaded ? (<GoogleMap
-                id="marker-example"
-                zoom={20}
-                mapContainerStyle={{height: "400px", width: "100%"}}
-                onLoad={onLoad}
-                onUnmount={onUnmount}
-                center={center}
-            >
-                <div className='w-1/2 mx-auto mt-[20px]' dir={strings.getLanguage() === "ar" ? "rtl ": "ltr"}><GooglePlacesAutocomplete
+        setCenter(loc)
+
+    }
+
+    return (<div className="p-5 mb-5 relative">
+        <button className="px-[100px] py-4 absolute  bottom-0 z-2 left-[25%] bg-blue-500 text-white rounded-2"> confirm</button>
+        {isLoaded ? (<GoogleMap
+            id="marker-example"
+            zoom={10}
+            mapContainerStyle={{ height: "400px", width: "100%" }}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+            center={center}
+            onClick={(event) => { placeMarker(event.latLng.toJSON()); }}
+            options={{
+                mapTypeControl: false, // Disable map type control
+            }}
+        >
+            <div className='w-[65%] mx-auto mt-[20px]' dir={strings.getLanguage() === "ar" ? "rtl " : "ltr"}>
+                <GooglePlacesAutocomplete
                     apiKey={"AIzaSyBhs9awrQC82lygPiy4Cq91xyX9s3WUjUI"} selectProps={{
-                    value, onChange: setValue,
-                }}/></div>
-                <Marker position={center}
-                        draggable={true}
-                        onDragEnd={(e) => {
-                            setCenter({lat: e.latLng.lat(), lng: e.latLng.lng()})
-                        }}
+                        value, onChange: setValue,
+                    }}
                 />
-            </GoogleMap>) : null}
-        </div>
+
+            </div>
+
+
+            <Marker position={center}
+                draggable={true}
+                onDragEnd={(e) => {
+                    setCenter({ lat: e.latLng.lat(), lng: e.latLng.lng() })
+                }}
+            />
+
+        </GoogleMap>) : null}
+    </div>
 
     );
 }
